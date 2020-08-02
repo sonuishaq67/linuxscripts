f40f3d20bca974b4cfabf3f32bb41d3043d27511
sonuishaq67 <sonuishaq67@gmail.com>
2020-08-02 22:00:01 +0530

    Sunday 02 August 2020 10:00:01 PM IST

--- change grub background/main.sh
+++ change grub background/main.sh
@@ -6,17 +6,20 @@ echo "This script must be run with sudo priveleges"
 exit 1
 fi
 
+
 case "$1" in --set)
 echo "
 please note that we are now going to set the grub screen 
 Do you want to proceed? [y/n] Type n to exit, or press enter to proceed.
 "
 read answer 
-if [ "$answer"=="n" ] || [ "$answer"=="N" ]
-then 
-echo "ok Exitting.."
+
+if [ "$answer" == "n" ] || [ "$answer" == "N" ]
+then
+echo "Exiting..."
 exit 1
 fi
+
 
 echo "
 Enter the path to the picture 
@@ -26,5 +29,44 @@ Example 2 : ~Pictures/grub.png
 "
 
 read path
-echo "GRUB_BACKGROUND="$path""
+echo "GRUB_BACKGROUND="$path"" >> /etc/default/grub >> grubimage.log
+
+update-grub >> grubimage.log
+
+echo "
+			       ===============
+			       |             |
+			       |  SUCCESSFUL |
+			       |             |
+			       ===============
+"
+;;
+
+--reset)
+echo "
+please note that we are now going to reset the grub screen 
+Do you want to proceed? [y/n] Type n to exit, or press enter to proceed.
+"
+read answer
+if [ "$answer"=="n" ] || [ "$answer"=="N" ]
+then
+echo "ok Exitting.."
+exit 1
+fi
+
+sed 's/GRUB_BACKGROUND/#GRUB_BACKGROUND/' /etc/default/grub >> grubimage.log
+update-grub >> grubimage.log
+echo "
+                               =====================
+                               |                   |
+                               |  RESET SUCCESSFUL |
+                               |                   |
+                               =====================
+"
+;;
+*)
+
+echo "Use the parameter '--set' or '--reset'; example: 'sudo ./main.sh --set' or 'sudo ./main.sh --reset'"
+exit 1
+esac
 
