17e1085e0650b3ab8c7304973053927a8a8afc1b
si67 <sonuishaq67@gmail.com>
2020-11-18 22:05:26 +0530

    added arch scriptas

--- battery-low-full/main.sh
+++ battery-low-full/main.sh
@@ -8,11 +8,11 @@ echo $ac_power
 if [[ $ac_power -eq 1 && $battery_level -eq 100 ]] #if charging and battery_level==100 
 then
 export DISPLAY=:0.0 
-zenity --warning --title="Battery level $battery_level" --width=600 --timeout=10 --text="Please remove the charger" | espeak "Charge full, Please Remove the charger" -s 140 
+zenity --warning --title="Battery level $battery_level" --width=600 --timeout=10 --text="Please remove the charger" | /usr/bin/espeak "Charge full, Please Remove the charger" -s 140 
 fi
 
 if [[ $ac_power -eq 0 && $battery_level -lt 20 ]] 
 then
 export DISPLAY=:0.0 
-zenity --warning --title="Battery level $battery_level" --width=600 --timeout=10 --text="Please plug the charger" | espeak "Please connect the charger" -s 140 
+zenity --warning --title="Battery level $battery_level" --width=600 --timeout=10 --text="Please plug the charger" | /usr/bin/espeak "Please connect the charger" -s 140 
 fi
